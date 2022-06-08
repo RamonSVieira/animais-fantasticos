bed38046adf2e9df02971312cc44eed6c4e28123
Ramon Vieira <ramonvieira122333@gmail.com>
2022-06-08 20:37:21 -0300

    sections appearing smoothly

--- assets/script.js
+++ assets/script.js
@@ -60,6 +60,29 @@ function initScrollSoft() {
   });
 }
 
+function initAnimationScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.7;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+
+        if (sectionTop < 0) {
+          section.classList.add("show");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
 initAccordion();
 initTabNav();
 initScrollSoft();
+initAnimationScroll();
