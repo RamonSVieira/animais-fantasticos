66dd6091c680f46181b199ac69bd61dfe961b8cf
Ramon Vieira <ramonvieira122333@gmail.com>
2022-08-19 16:44:29 -0300

    adding login functionality via a modal

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,9 @@ import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
 import initScrollSoft from "./modules/scroll-smoothly.js";
 import initAnimationScroll from "./modules/scroll-animation.js";
+import initModal from "./modules/modal.js";
 
+initModal();
 initAccordion();
 initTabNav();
 initScrollSoft();
